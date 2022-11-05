adad9398b8f84a9289b2b455f511447422f00024
Ivan da Silveira <ivandasilveira@Ivans-MacBook-Pro.local>
2022-11-05 10:12:30 +0100

    changes

---
+++ packages/compiler/src/bin.js
@@ -0,0 +1,33 @@
+
+import { Command } from 'commander'
+// import { bundle } from './bundle';
+import { spawn } from './run';
+
+const program = new Command();
+
+program.name('compiler');
+
+program
+	.command('run')
+	.argument('<file>', 'file to execute')
+	.description('execute a file')
+	.option('-e, --env <variables...>', 'space separated environment variables')
+	.option('--include-packages', 'include all packages inside the build process')
+	.action(async (input, options) => {
+		const node = await spawn(input, options);
+		node.stdout.pipe(process.stdout);
+		node.stderr.pipe(process.stderr);
+	});
+
+// program
+// 	.command('bundle')
+// 	.argument('<file>', 'file to bundle')
+// 	.description('execute a coffeescript file')
+// 	.option('--include-packages', 'include all packages inside the build process')
+// 	.action(async (input, options) => {
+// 		const node = await bundle(input, options);
+// 		node.stdout.pipe(process.stdout);
+// 		node.stderr.pipe(process.stderr);
+// 	});
+
+program.parse(process.argv);
