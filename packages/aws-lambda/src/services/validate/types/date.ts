9d4acca3c9e7ecdccc814ed80f8ef3403ecfb63d
Michel Monen <michel@monen.com>
2022-12-14 14:58:47 +0100

    changes

--- packages/aws-lambda/src/services/validate/types/date.ts
+++ packages/aws-lambda/src/services/validate/types/date.ts
@@ -2,7 +2,7 @@
 import { coerce, date as sdate, string, Struct } from 'superstruct'
 
 export const date = ():Struct<Date, null> => {
-	return coerce(sdate(), string(), (value) => {
+	return coerce(sdate(), string(), (value: Date | string) => {
 		return new Date(value)
 	})
 }
--- packages/aws-lambda/src/services/validate/validate.ts
+++ packages/aws-lambda/src/services/validate/validate.ts
@@ -2,11 +2,15 @@
 import { Struct, StructError, mask } from 'superstruct'
 import { ValidationError } from './error.js'
 
+const isStructError = (error: unknown): error is StructError => {
+	return error instanceof StructError
+}
+
 export const validate = <T, S>(value: unknown, struct: Struct<T, S>): T => {
 	try {
 		return mask(value, struct)
 	} catch(error) {
-		if(error instanceof StructError) {
+		if(isStructError(error)) {
 			throw new ValidationError(error.failures())
 		}
 
